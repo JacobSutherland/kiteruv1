8cccfa4c48487656c050119fe4a61c0328da6edf
JacobSutherland <jacobsutherland224@gmail.com>
2021-05-03 02:10:23 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,67 @@
+const body = document.querySelector('body');
+const game = document.querySelector('.card-section');
+const button = document.querySelector('button');
+const cardSection = document.querySelector('.card-section');
+
+let questionRandomizer = () => {
+    const roll = Math.floor(Math.random() * characters.length);
+    return roll;
+}
+
+let questionCounter = 0;
+
+button.addEventListener('click', () => {
+    const question = questionRandomizer();
+    questionCounter += 1;
+    console.log(questionCounter)
+    
+    //creates and displays a div element containing the random question
+    const card = document.createElement('div');
+    card.classList.add('card');
+    const currentQuestion = document.createElement('h1');
+    currentQuestion.textContent = `${characters[question].char}`
+    card.appendChild(currentQuestion);
+
+    //creates and populates answer div with random kana sounds
+    const options = document.createElement('div');
+    options.classList.add('answer-container');
+    //create a p element 4 times and add 'answer' class
+    for(let i = 0; i < 4; i++){
+        const option = document.createElement('p');
+        option.classList.add('answer');
+        //the first answer option will be correct, while the subsequent options will be randomized
+        if(i < 1){
+        option.textContent = `${characters[question].sound}`
+        } else {
+        option.textContent = `${characters[questionRandomizer()].sound}`
+        }
+        //finally appends p element at the end of an itteration
+        options.appendChild(option);
+    }
+
+    //appends game elements, removes the large PNG once game starts, and updates button text
+    cardSection.removeChild(cardSection.firstElementChild);
+        button.textContent = "Next Kana"
+        game.appendChild(card);
+        game.appendChild(options);
+
+        //removes the last set of questions and answers
+        if(questionCounter > 1){
+            cardSection.removeChild(cardSection.firstElementChild);
+        }
+
+        //checks if the answer is correct when clicked
+        const answer = document.querySelectorAll('.answer');
+        answer.forEach( i => {
+
+            i.addEventListener('click', (e) => {
+                if(e.target.textContent === characters[question].sound){
+                    i.style.backgroundColor = '#22eaaa'
+                    i.style.border = 'none'
+                }
+            })
+        })
+
+})
+
+
---
+++ data.js
@@ -0,0 +1,227 @@
+const characters = [
+    {
+        char: 'あ',
+        sound: 'a',
+        id: 1,
+    },
+    {
+        char: 'い',
+        sound: 'i',
+        id: 2,
+    },
+    {
+        char: 'う',
+        sound: 'u',
+        id: 3,
+    },
+    {
+        char: 'え',
+        sound: 'e',
+        id: 4,
+    },
+    {
+        char: 'お',
+        sound: 'o',
+        id: 6,
+    },
+    {
+        char: 'か',
+        sound: 'ka',
+        id: 7,
+    },
+    {
+        char: 'き',
+        sound: 'ki',
+        id: 8,
+    },
+    {
+        char: 'く',
+        sound: 'ku',
+        id: 9,
+    },
+    {
+        char: 'け',
+        sound: 'ke',
+        id: 10,
+    },
+    {
+        char: 'こ',
+        sound: 'ko',
+        id: 11,
+    },
+    {
+        char: 'さ',
+        sound: 'sa',
+        id: 12,
+    },
+    {
+        char: 'し',
+        sound: 'shi',
+        id: 13,
+    },
+    {
+        char: 'す',
+        sound: 'su',
+        id: 14,
+    },
+    {
+        char: 'せ',
+        sound: 'se',
+        id: 15,
+    },
+    {
+        char: 'そ',
+        sound: 'so',
+        id: 16,
+    },
+    {
+        char: 'た',
+        sound: 'ta',
+        id: 17,
+    },
+    {
+        char: 'ち',
+        sound: 'chi',
+        id: 18,
+    },
+    {
+        char: 'つ',
+        sound: 'tsu',
+        id: 19,
+    },
+    {
+        char: 'て',
+        sound: 'te',
+        id: 20,
+    },
+    {
+        char: 'と',
+        sound: 'to',
+        id: 21,
+    },
+    {
+        char: 'な',
+        sound: 'na',
+        id: 22,
+    },
+    {
+        char: 'に',
+        sound: 'ni',
+        id: 23,
+    },
+    {
+        char: 'ぬ',
+        sound: 'nu',
+        id: 24,
+    },
+    {
+        char: 'ね',
+        sound: 'ne',
+        id: 25,
+    },
+    {
+        char: 'の',
+        sound: 'no',
+        id: 26,
+    },
+    {
+        char: 'は',
+        sound: 'ha',
+        id: 27,
+    },
+    {
+        char: 'ひ',
+        sound: 'hi',
+        id: 28,
+    },
+    {
+        char: 'ふ',
+        sound: 'fu',
+        id: 29,
+    },
+    {
+        char: 'へ',
+        sound: 'he',
+        id: 30,
+    },
+    {
+        char: 'ほ',
+        sound: 'ho',
+        id: 31,
+    },
+    {
+        char: 'ま',
+        sound: 'ma',
+        id: 32,
+    },
+    {
+        char: 'み',
+        sound: 'mi',
+        id: 33,
+    },
+    {
+        char: 'む',
+        sound: 'mu',
+        id: 34,
+    },
+    {
+        char: 'め',
+        sound: 'me',
+        id: 35,
+    },
+    {
+        char: 'も',
+        sound: 'mo',
+        id: 36,
+    },
+    {
+        char: 'や',
+        sound: 'ya',
+        id: 37,
+    },
+    {
+        char: 'ゆ',
+        sound: 'yu',
+        id: 38,
+    },
+    {
+        char: 'よ',
+        sound: 'yo',
+        id: 39,
+    },
+    {
+        char: 'ら',
+        sound: 'ra',
+        id: 40,
+    },
+    {
+        char: 'り',
+        sound: 'ri',
+        id: 41,
+    },
+    {
+        char: 'る',
+        sound: 'ru',
+        id: 42,
+    },
+    {
+        char: 'れ',
+        sound: 're',
+        id: 43,
+    },
+    {
+        char: 'ろ',
+        sound: 'ro',
+        id: 44,
+    },
+    {
+        char: 'わ',
+        sound: 'wa',
+        id: 45,
+    },
+    {
+        char: 'ゐ',
+        sound: 'wi',
+        id: 46,
+    }
+]
